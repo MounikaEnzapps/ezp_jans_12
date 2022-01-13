ecbfb7b7d98ca945cb03b8e52d8a7e47782bd31c
USER <yarmiztechnlogies@gmail.com>
2022-01-13 19:34:12 +0530

    <MounikaEnzapps>

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Enz Rehla JAN",
+    'author':
+        'Enzapps',
+    'summary': """
+This module will help to payment by Check.
+""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+    'website': "",
+    'category': 'base',
+    'version': '12.0',
+    'depends': ['base','sale','purchase','ezp_rehlacar'],
+    "images": ['static/description/icon.png'],
+    'data': [
+        'views/sale.xml',
+
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+}
